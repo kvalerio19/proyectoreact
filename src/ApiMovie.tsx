28f4abcc1b5b53c0cd017d7f91ac2c76aa85a69d
kvalerio19 <kvalerio19@hotmail.es>
2021-10-26 21:43:48 -0600

    Initial commit

---
+++ src/ApiMovie.tsx
@@ -0,0 +1,56 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+
+
+function ApiMovie() {
+    const API_URL_UPCOMING = 'https://api.themoviedb.org/3/movie/upcoming?api_key=f6f6ccdecf4c60c8bb20d597133d740e&language=en-US&page=1';
+    const API_URL_TOP_RATED_TV_SHOWS = 'https://api.themoviedb.org/3/tv/top_rated?api_key=f6f6ccdecf4c60c8bb20d597133d740e&language=en-US&page=1';
+    const API_POPULAR_MOVIES = 'https://api.themoviedb.org/3/movie/popular?api_key=f6f6ccdecf4c60c8bb20d597133d740e&language=en-US&page=1';
+    const API_RATE_MOVIES = 'https://api.themoviedb.org/3/movie/{movie_id}/rating?api_key=f6f6ccdecf4c60c8bb20d597133d740e';
+
+    const [movies, setMovies] =useState([]);
+    const [rating, setRating] = useState(0);
+
+    //usa datos hasta que se haya cargado el componente completamente
+
+    useEffect(() =>{
+        const fetchData = async () =>{
+            const {data} = await axios.get(API_URL_UPCOMING);
+            console.log(data);
+          
+            setMovies(data.results);
+            
+        };
+        fetchData();
+    },[]);
+
+    useEffect(() =>{
+        const fetchData = async () =>{
+            const {data} = await axios.get(API_URL_TOP_RATED_TV_SHOWS);
+            console.log(data);
+          
+            setRating(data.results);
+            
+        };
+        fetchData();
+    },[]);
+    
+    return (
+        <div>
+              {movies.map((movie:any)=>(
+                  <div key={movie.id}>
+                      <h1>{movie.title}</h1>
+                      <p>{movie.description}</p>
+                      <img src={'https://image.tmdb.org/t/p/w500' + movie.backdrop_path} />
+                  </div>
+              ))}
+              
+        </div>
+
+        
+    )
+
+
+}
+
+export default ApiMovie
